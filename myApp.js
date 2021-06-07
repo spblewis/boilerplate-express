e20daacec29357460c49a61b1f896f5e33ad958a
Schuyler Lewis <>
2021-06-07 23:15:32 +0000

    added first argument to app.use

--- myApp.js
+++ myApp.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+  
     res.sendFile(__dirname + '/views/index.html');
 });
 
