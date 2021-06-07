e1dbef52a1d5c75ef8bb5bd64c2c28698e8a1777
spblewis <spblewis@gmail.com>
2021-06-07 18:19:44 -0500

    serving some json

--- myApp.js
+++ myApp.js
@@ -4,11 +4,12 @@ var app = express();
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  
     res.sendFile(__dirname + '/views/index.html');
 });
 
-
+app.get('/json', (req, res) => {
+    res.json({"message": "Hello json"});
+});
 
 
 
