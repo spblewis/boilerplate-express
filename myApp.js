664f5a985df3649ee1499f918405942d5fba50dd
spblewis <spblewis@gmail.com>
2021-06-08 10:30:39 -0500

    Added chained middleware to add date to a req

--- myApp.js
+++ myApp.js
@@ -17,7 +17,12 @@ app.get('/json', (req, res) => {
       "HELLO JSON" : "Hello json")});
 });
 
-
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({time: req.time});
+})
 
 
 
