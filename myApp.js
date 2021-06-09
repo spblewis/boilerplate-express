56b437ac3b9769ed3b9f0497bf1b23b6c903f31c
spblewis <spblewis@gmail.com>
2021-06-09 15:17:25 -0500

    processing a query string

--- myApp.js
+++ myApp.js
@@ -28,7 +28,9 @@ app.get('/:word/echo', (req, res) => {
     res.json({echo: req.params.word});
 });
 
-
+app.get('/name', (req, res) => {
+    res.json({ name: `${req.query.first} ${req.query.last}`})
+})
 
 
 
